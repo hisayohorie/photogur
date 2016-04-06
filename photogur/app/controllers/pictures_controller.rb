a9f782cb28b80c49a5a92eebf2fdcab5dce1fef1
HISAYO <hisayohorie@gmail.com>
2016-04-06 15:40:43 -0400

    refactoring views to use helpers and adding navigations

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -8,7 +8,18 @@ class PicturesController < ApplicationController
   end
 
   def create
-    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]},
-    Artist: #{params[:artist]}"
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to picture_url
+    else
+      render :new
+    # render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]},
+    # Artist: #{params[:artist]}"
+  end
+end
+
+private
+  def picture_params
+    params.require(:picture).permit(:artist,:title, :url)
   end
 end
